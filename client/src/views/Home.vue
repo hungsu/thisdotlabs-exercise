fe1a19879fe5dbc376f837c6ca8278db097f38e8
Hung-Su Nguyen <hungsu.nguyen@gmail.com>
2020-12-13 13:42:31 -0800

    Add simple pagination

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="home">
     <form v-on:submit.prevent="onSubmit">
-      <input type="search" v-model="query">
+      <label for="user-search"> Search for Github users:</label>
+      <input id="user-search" type="search" v-model="query" placeholder="example: arthyn">
+      <input type="submit" value="🔍">
     </form>
     <Results :results="results" />
-
+    <div v-if="pageTotal">
+      <button v-if="pageNumber > 1" @click="getPrevious">Previous</button>
+      Page {{pageNumber}} of {{pageTotal}}
+      <button v-if="pageNumber < pageTotal" @click="getNext">Next</button>
+    </div>
     <p v-if="previousQuery.length > 0">{{ resultsCountString }}</p>
   </div>
 </template>
@@ -17,6 +23,7 @@ import Axios from "axios"
 @Component({
   data: function () {
     return {
+      pageNumber: 1,
       previousQuery: '',
       query: '', // Up to 256 characters https://docs.github.com/en/free-pro-team@latest/rest/reference/search#limitations-on-query-length
       results: [],
@@ -27,6 +34,9 @@ import Axios from "axios"
     Results
   },
   computed: {
+    pageTotal: function () {
+      return Math.ceil(this.resultCount / 30)
+    },
     resultsCountString: function() {
       const noun = this.resultCount > 1 ? 'results' : 'result'
       return `${this.resultCount} ${noun} found.`
@@ -34,12 +44,25 @@ import Axios from "axios"
   },
   methods: {
     onSubmit: function() {
-      const url = 'https://api.github.com/search/users?type=user&q=' + this.query
+      this.pageNumber = 1
+      this.getResults(this.query)
+    },
+    getNext: function () {
+      this.pageNumber++
+      this.getResults(this.query)
+    },
+    getPrevious: function () {
+      this.pageNumber--
+      this.getResults(this.query)
+    },
+    getResults: function (query) {
+      const url = `https://api.github.com/search/users?type=user&page=${this.pageNumber}&q=${query}`
+
       const config = {}
       Axios.get(url,config).then(response => {
         this.results = response.data.items
         this.resultCount = 0 + response.data.total_count
-        this.previousQuery = this.query
+        this.previousQuery = query
       })
     }
   }
